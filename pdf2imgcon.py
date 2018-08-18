f570ad189c25999d71649bd475963391085c60f6
Prashanthi Bojanapu <prashanthi.bojanapu@gmail.com>
2018-08-18 14:21:08 +0530

    Add files via upload

--- pdf2imgcon.py
+++ pdf2imgcon.py
@@ -1,18 +1,19 @@
 # This project is written in python 3.6 
-#The purpose of this project is to convert the list of pdfs to images 
+#The purpose of this project is to convert the list of pdfs in a directory to images
+#output images will be saved in images folder
 
 #importing the required libraries
-
-import os, sys, importlib, shutil
-import time
-import pandas as pd
 import os
+import sys
+import io
+import time
+
+import pandas as pd
 import PyPDF2
 from PyPDF2 import PdfFileReader
 from wand import image 
 from wand.image import Image
 from wand.color import Color
-import io
 
 def pdf_page_to_png(src_pdf, pagenum = 0, resolution = 100,):
     ''' convert the each page in pdf to image'''
@@ -23,7 +24,6 @@ def pdf_page_to_png(src_pdf, pagenum = 0, resolution = 100,):
     pdf_bytes = io.BytesIO()
     dst_pdf1.write(pdf_bytes)
     pdf_bytes.seek(0)
-
     img =image.Image(file = pdf_bytes, resolution = resolution)
     img.background_color = Color('white')
     img.alpha_channel = 'remove'
@@ -35,20 +35,18 @@ def pdf_page_to_png(src_pdf, pagenum = 0, resolution = 100,):
 def pdftoimage_conversion(pdfs_path,resol):
     ''' Takes input pdfs path and convert the pdf to images and save in the directiory images'''
     try:
-        path =  pdfs_path
-        dir_path = os.path.join(path,'images')
+        dir_path = os.path.join(pdfs_path,'images')
         if not (os.path.exists(dir_path)):
             os.makedirs(dir_path)
         start_time = time.time()
         files = [f for f in os.listdir( pdfs_path) if f.lower().endswith((".pdf"))]
         for f in files:
             pdf=PdfFileReader(open(os.path.join( pdfs_path, f),'rb'))
-            for i in range(1,pdf.getNumPages()):
+            for i in range(pdf.getNumPages()):
                 file_name ="{0}_{1}.jpg".format(f[:-4],i)
                 dir_path_file = os.path.join(dir_path,file_name)
                 if not os.path.isfile(dir_path_file):
                     img = pdf_page_to_png(pdf, pagenum = i, resolution = resol)
-                    output = open(os.path.join(dir_path, file_name), 'wb')
                     img.save(filename = (os.path.join(dir_path, file_name)))
         end_time = time.time()
         print("Done!!!!")
@@ -61,15 +59,18 @@ def pdftoimage_conversion(pdfs_path,resol):
         return response
     
 if __name__ == '__main__':
+    
+    if len(sys.argv) < 2:
+        print('Usage: python pdf2imgcon.py path to pdf files resolution_between 100 to 500\nexample python pdf2imgcon.py path/pdfs 100')
+        sys.exit(1)
     pdfs_path = sys.argv[1]
-    resol = input("Enter the resolution for the images between 100 and 500 :: ")
-    if type(resol) == int:
-        if 100 >= resol <=500:
-            resol = resol
+    resol = int(sys.argv[2])
+    res_check = False
+    while not res_check:
+        if resol >= 100 and resol<= 500:
+            res_check = True
+            pdftoimage_conversion(pdfs_path,resol)
         else:
-            print("Default resolution taken")
-            resol = 100
-    else:
-        print("Default resolution taken")
-        resol = 100
-    pdftoimage_conversion(pdfs_path,resol)
+            print("please enter the resolution between 100 and 500\n")
+            resol = int(input("Enter the resolution for the images between 100 and 500 :: "))
+    
